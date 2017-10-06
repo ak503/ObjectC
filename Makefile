2a6cb1606fa517e062909474fa94aa3d296e3e27
dturlupov <dturlupov@factor-ts.ru>
2017-10-06 13:59:01 +0300

    str 35

--- Makefile
+++ Makefile
@@ -3,7 +3,12 @@ CFLAGS1 = `gnustep-config --objc-flags`
 CFLAGS2 = `gnustep-config --base-libs`
 CFLAGS = $(CFLAGS1) $(CFLAGS2)
 
-test:
-	$(CC) main.m $(CFLAGS) -o test
+PROJECT = main
+SOURCES = main.m
+$(PROJECT): $(SOURCES)
+
+
+main:
+	$(CC) $(SOURCES) $(CFLAGS) -o test
 clean:
 	rm -f test test.d test.o *~
--- main.m
+++ main.m
@@ -1,23 +1,74 @@
-//#import
 #import <Foundation/Foundation.h>
+
+@interface Fraction: NSObject {
+	int numerator;
+	int denumerator;
+}
+
+-(void) print;
+-(void) setNum: (int) a;
+-(void) setDenum: (int) b;
+-(int) getNum;
+-(int) getDenum;
+//-(int) getSumm;
+@end
+
+@implementation Fraction: NSObject
+-(void) print 
+{
+	NSLog(@"num=%i; denum=%i;", numerator,denumerator);
+}
+
+-(void) setNum: (int) a
+{
+	numerator = a;
+}
+
+-(void) setDenum: (int) b
+{
+	denumerator = b;
+}
+
+-(int) getNum
+{
+	return numerator;
+}
+
+-(int) getDenum
+{
+	return denumerator;
+}
+
+/*
+-(double) getRet
+{
+	return numerator / denumerator;
+}*/
+@end
 
 int main (int argc, const char * argv[])
 {
+	//int a = 15;
+	//int b = 87;
+	/*
+	[ Class metod ] ;
+	*/
+	//NSLog(@"%i - %i = %i", b,a, b-a);
 	//NSLog (@"Hello world");
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-	NSLog(@"Hello, World");
+
+	Fraction *testClass;
+	testClass = [Fraction alloc];
+	testClass = [testClass init];
+	[testClass setNum: 1];
+	[testClass setDenum: 2];
+	int num = [testClass getNum] ;
+	NSLog(@"num: %2i", num);
+	int denum = [testClass getDenum] ;
+	NSLog(@"denum: %i", denum);
+	//[testClass print];
+	[testClass release];
+	NSLog(@"rez: %d", num/denum);
 	[pool drain];
 	return 0;
 }
-//main.m
-/*
-#import <Foundation/Foundation.h>
-
-int main (int argc, const char * argv[])
-{
-        NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-        NSLog (@"hello world");
-        [pool drain];
-        return 0;
-}*/
